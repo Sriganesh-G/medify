7e1d2508a3b1b1cc8e52b1b896ca985b5f2f7854
Sriganesh-G <sriganesh9647@gmail.com>
2024-09-16 20:23:15 +0530

    added navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { styled } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid2";
+import { Button, Typography } from "@mui/material";
+import logo from "../../public/assets/logo.png";
+import "../App.css";
+
+const Navbar = () => {
+  return (
+    <div style={{ height: "96px", width: "100%" }}>
+      <Grid
+        container
+        spacing={2}
+        sx={{
+          display: "flex",
+          justifyContent: "",
+          alignItems: "center",
+          // padding: "0px 20px 0px 50px",
+          // pl: "50px",
+          pr: "30px",
+          height: "96px",
+          width: "100%",
+        }}
+      >
+        <Grid
+          size={2.6}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <img src={logo} alt="Logo" />
+          <Typography
+            sx={{
+              color: "#2AA8FF",
+              pl: "5px",
+              //  fontSize: "18px",
+              fontWeight: "bold",
+            }}
+            variant="h6"
+          >
+            Medify
+          </Typography>
+        </Grid>
+        <Grid size={1.33}>
+          <Typography>Find Doctors</Typography>
+        </Grid>
+        <Grid size={1.33}>
+          <Typography>Hospitals</Typography>
+        </Grid>
+        <Grid size={1.33}>
+          <Typography>Medicines</Typography>
+        </Grid>
+        <Grid size={1.33}>
+          <Typography>Surgeries</Typography>
+        </Grid>
+        <Grid size={1.36}>
+          <Typography>Software for Provider</Typography>
+        </Grid>
+        <Grid
+          size={1.33}
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Typography>Facilities</Typography>
+        </Grid>
+        <Grid size={1.33}>
+          <button
+            style={{
+              color: "white",
+              fontSize: "14px",
+              backgroundColor: "#2AA8FF",
+              height: "40px",
+              padding: "20px",
+              border: "none",
+              borderRadius: "8px",
+              display: "flex",
+              justifyContent: "center",
+              alignItems: "center",
+              cursor: "pointer",
+            }}
+          >
+            My Bookings
+          </button>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,14 @@
 import React from "react";
 import { Typography } from "@mui/material";
-
+import HeaderMessage from "../components/HeaderMessage";
+import Navbar from "../components/Navbar";
+import "../App.css";
 const Home = () => {
   return (
     <div>
       {/* header-message */}
-      <div
-        style={{
-          backgroundColor: "#2AA7FF",
-          height: "40px",
-          width: "100%",
-          color: "#FFFFFF",
-          textAlign: "center",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography sx={{ fontSize: { xs: "8px", sm: "12px", md: "16px" } }}>
-          The health and well-being of our patients and their health care team
-          will always be our priority, so we follow the best practices for
-          cleanliness.
-        </Typography>
-      </div>
-      
+      <HeaderMessage />
+      <Navbar />
     </div>
   );
 };
